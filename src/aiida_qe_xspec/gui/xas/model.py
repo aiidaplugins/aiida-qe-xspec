aiidaplugins/aiida-qe-xspec

commit 696c6b5ba32e8568619c988184a65f71ff4d04ec

General Update: Import Paths, Namespaces, and Binding Energy Settings
Changes:
 - Update import paths.
 - Change namespace names for `PwRelaxWorkChain` (now compatible with aiida-qe v5.0.x).
 - Make optional the absolute `binding_energies` and relative output `binding_energy_spectra`.

src/aiida_qe_xspec/workflows/xps.py
@@ -355,6 +355,7 @@ class XpsWorkChain(ProtocolMixin, WorkChain):
         spec.output(
             'binding_energies',
             valid_type=orm.Dict,
+            required=False,
             help='All the binding energy values for each element calculated by the WorkChain.'
         )
         spec.output_namespace(
@@ -366,6 +367,7 @@ class XpsWorkChain(ProtocolMixin, WorkChain):
         spec.output_namespace(
             'binding_energy_spectra',
             valid_type=orm.XyData,
+            required=False,
             dynamic=True,
             help='The fully-resolved spectra for each element based on binding energy.'
         )
@@ -543,7 +545,8 @@ class XpsWorkChain(ProtocolMixin, WorkChain):
                 kpoints_mesh = DataFactory('core.array.kpoints')()
                 kpoints_mesh.set_kpoints_mesh([1, 1, 1])
                 builder.ch_scf.kpoints = kpoints_mesh
-                builder.relax.base.pw.settings = orm.Dict(dict={'gamma_only': True})
+                builder.relax.base_init_relax.pw.settings = orm.Dict(dict={'gamma_only': True})
+                builder.relax.base_relax.pw.settings = orm.Dict(dict={'gamma_only': True})
         # pylint: enable=no-member
         return builder
 

src/aiida_qe_xspec/calculations/xspectra.py
@@ -27,7 +27,7 @@ class XspectraCalculation(NamelistsCalculation):
     _internal_retrieve_list = [_Spectrum_FILENAME]
     _retrieve_singlefile_list = []
     _retrieve_temporary_list = []
-    _default_parser = 'quantumespresso.xspectra'
+    _default_parser = 'xspec.xspectra'
 
     @classmethod
     def define(cls, spec):
@@ -131,7 +131,7 @@ class XspectraCalculation(NamelistsCalculation):
         parent_folder = self.inputs.parent_folder
         parent_calc = parent_folder.creator
 
-        if parent_calc.process_type == 'aiida.calculations:quantumespresso.xspectra':
+        if parent_calc.process_type == 'aiida.calculations:xspec.xspectra':
             calcinfo.remote_copy_list.append((
                 parent_folder.computer.uuid, os.path.join(parent_folder.get_remote_path(),
                                                           self._XSPECTRA_SAVE_FILE), '.'

src/aiida_qe_xspec/workflows/functions/get_xspectra_structures.py
@@ -136,7 +136,7 @@ def get_supercell(structure, supercell_min_parameter, is_hubbard_structure, **kw
     blank_supercell = StructureData(ase=ase_supercell)
     new_supercell = StructureData()
     new_supercell.set_cell(blank_supercell.cell)
-    num_extensions = np.product(multiples)
+    num_extensions = np.prod(multiples)
     types_order = kwargs['types_order']
     type_mapping_dict = kwargs['type_mapping_dict']
     supercell_types_order = []

src/aiida_qe_xspec/workflows/xspectra/crystal.py
@@ -9,7 +9,7 @@ from aiida.orm import UpfData as aiida_core_upf
 from aiida.plugins import CalculationFactory, DataFactory, WorkflowFactory
 from aiida_pseudo.data.pseudo import UpfData as aiida_pseudo_upf
 
-from aiida_quantumespresso.calculations.functions.xspectra.get_spectra_by_element import get_spectra_by_element
+from aiida_qe_xspec.calculations.functions.xspectra.get_spectra_by_element import get_spectra_by_element
 from aiida_quantumespresso.utils.hubbard import HubbardStructureData
 from aiida_quantumespresso.utils.mapping import prepare_process_inputs
 from aiida_quantumespresso.workflows.protocols.utils import ProtocolMixin, recursive_merge

commit defabfbb7fbcef28ef0d6548318c20ff8556c85f

GUI: Update AiiDALab-QE Plugin for new CH Treatment Scheme
Makes necessary changes to the `gui` element of the plugin in order to
enable correct usage of the new core-hole treatment scheme.

Changes:
 * Updated core-hole treatment options added to UI, with minor changes
   to help texts to improve user experience.
 * Deals with an oversight in `process_molecule` function of
   `get_xspectra_structures` where only the symbol and not the
`kind_name` was returned to the symmetry data. This change means that
`get_core_hole_inputs` will use the site symbol if the symmetry data
does not give a `kind_name`.
 * Fixes an issue introduced in a recent commit
   (696c6b5ba32e8568619c988184a65f71ff4d04ec) caused by AiiDALab-QE only
being compatible with AiiDA-QE v4.12.1, thus triggering a crash due to
input ports for the `PwRelaxWorkChain` being given incorrectly.

pyproject.toml
@@ -21,7 +21,7 @@ classifiers = [
 ]
 dependencies = [
   'aiida-core>=2.6',
-  'aiida-quantumespresso>=4.10',
+  'aiida-quantumespresso==4.12.1',
   'gitpython'
 ]
 dynamic = ['description', 'version']

src/aiida_qe_xspec/gui/xas/model.py
@@ -35,9 +35,8 @@ class XasConfigurationSettingsModel(ConfigurationSettingsModel, HasInputStructur
     core_hole_treatments_options = tl.List(
         trait=tl.List(tl.Unicode()),
         default_value=[
-            ['FCH', 'full'],
-            ['XCH (Smearing)', 'xch_smear'],
-            ['XCH (Fixed)', 'xch_fixed'],
+            ['Full', 'full'],
+            ['Excited', 'excited'],
         ],
     )
     core_hole_treatments = tl.Dict(
@@ -102,7 +101,7 @@ class XasConfigurationSettingsModel(ConfigurationSettingsModel, HasInputStructur
         return list(self.kind_names)
 
     def get_recommendation(self, element):
-        return 'xch_smear' if element in self.xch_elements else 'full'
+        return 'Excited' if element in self.xch_elements else 'Full'
 
     def reset(self):
         with self.hold_trait_notifications():

src/aiida_qe_xspec/gui/xas/setting.py
@@ -76,25 +76,19 @@ class XasConfigurationSettingsPanel(
                             Select the core-hole treatment scheme from the dropdown box
                         </li>
                     </ol>
-                    There are three supported options for core-hole treatment:
+                    There are two supported options for core-hole treatment:
                     <ul>
                         <li>
-                            FCH: Remove one electron from the system (any occupations
-                            scheme)
+                            FCH: Remove one electron from the system
                         </li>
                         <li>
-                            XCH (Smearing): places the excited electron into the
-                            conduction band (smeared occupations)
-                        </li>
-                        <li>
-                            XCH (Fixed): places the excited electron into the conduction
-                            band (fixed occupations).
+                            XCH: places the excited electron into the conduction band
                         </li>
                     </ul>
                     <p style="margin-bottom: 10px;">
-                        For XAS calculations of most elements, the FCH treatment is
-                        recommended, however in some cases the XCH treatment should be
-                        used instead.
+                        For XAS calculations of most light elements, the FCH treatment is
+                        recommended, however in some cases (e.g. transition metals) the
+                        XCH treatment should be used instead.
                     </p>
                     <p>
                         The recommended setting will be shown for each available element.
@@ -119,7 +113,7 @@ class XasConfigurationSettingsPanel(
                     Define the minimum cell length in angstrom for the resulting
                     supercell, and thus all output structures. The default value of 8.0
                     angstrom will be used if no input is given. Setting this value to
-                    0.0 will instruct the CF to not scale up the input structure.
+                    0.0 will not modify the input structure.
                 </div>
             """),
             ipw.HBox(

src/aiida_qe_xspec/gui/xps/model.py
@@ -18,12 +18,11 @@ class XpsConfigurationSettingsModel(PanelModel, HasInputStructure):
     core_hole_treatment_options = tl.List(
         trait=tl.List(tl.Unicode()),
         default_value=[
-            ['XCH(smear)', 'xch_smear'],
-            ['XCH(fixed)', 'xch_fixed'],
+            ['Excited', 'excited'],
             ['Full', 'full'],
         ],
     )
-    core_hole_treatment = tl.Unicode('xch_smear')
+    core_hole_treatment = tl.Unicode('excited')
     pseudo_group_options = tl.List(
         trait=tl.Unicode(),
         default_value=[

src/aiida_qe_xspec/gui/xps/workchain.py
@@ -37,16 +37,12 @@ def get_builder(codes, structure, parameters, **kwargs):
     for element, levels in correction_energies.items():
         for level, energy in levels.items():
             correction_energies[element][level] = energy - band_gap_correction
-    #
+    
     is_molecule_input = True if xps_parameters.get('structure_type') == 'molecule' else False
-    # set core hole treatment based on electronic type
-    if parameters['workchain']['electronic_type'] == 'metal':
-        core_hole_treatment = 'xch_smear'
-    else:
-        core_hole_treatment = 'xch_fixed'
-    # if molecule input, set core hole treatment to full
     if is_molecule_input:
         core_hole_treatment = 'full'
+    else:
+        core_hole_treatment = 'excited'
     core_hole_treatments = {element: core_hole_treatment for element in core_levels}
     structure_preparation_settings = {
         'supercell_min_parameter': Float(supercell_min_parameter_map[protocol]),

src/aiida_qe_xspec/workflows/functions/get_core_hole_inputs.py
@@ -52,7 +52,7 @@ def get_core_hole_inputs(structure, treatment, parameters, abs_site_data, **kwar
 
     abs_atom_marker = kwargs.get('abs_atom_marker', 'X')
     site_index = abs_site_data['site_index']
-    abs_atom_kind = abs_site_data['kind_name']
+    abs_atom_kind = abs_site_data.get('kind_name', abs_site_data['symbol'])
     updated_parameters = parameters.copy()
     starting_mag = parameters['SYSTEM'].get('starting_magnetization', None)
     if not starting_mag and parameters['SYSTEM'].get('nspin', None) == 2:

src/aiida_qe_xspec/workflows/xps.py
@@ -467,8 +467,11 @@ class XpsWorkChain(ProtocolMixin, WorkChain):
                 kpoints_mesh = DataFactory('core.array.kpoints')()
                 kpoints_mesh.set_kpoints_mesh([1, 1, 1])
                 builder.ch_scf.kpoints = kpoints_mesh
-                builder.relax.base_init_relax.pw.settings = orm.Dict(dict={'gamma_only': True})
-                builder.relax.base_relax.pw.settings = orm.Dict(dict={'gamma_only': True})
+                builder.relax.base.pw.settings = orm.Dict(dict={'gamma_only': True})
+                # These are the correct input ports for v5 of AiiDA-QE, but since AiiDALab-QE requires
+                # v4.12.1 this makes the GUI part of the plugin incompatable with v5 of AiiDA-QE.
+                # builder.relax.base_init_relax.pw.settings = orm.Dict(dict={'gamma_only': True})
+                # builder.relax.base_relax.pw.settings = orm.Dict(dict={'gamma_only': True})
         # pylint: enable=no-member
         return builder
 